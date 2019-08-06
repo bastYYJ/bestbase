c741822217e5db143b4448fff1fabd1f2b169f52
yyj <347485958@qq.com>
2019-08-06 14:00:20 +0800

    ..

--- app/src/main/java/com/yyj/bestbase/MApplication.java
+++ app/src/main/java/com/yyj/bestbase/MApplication.java
@@ -1,14 +1,13 @@
 package com.yyj.bestbase;
 
-import android.app.Application;
 import android.content.Context;
 
-public class MApplication extends Application {
+public class MApplication {
 
     /**
      * 更新方式
      */
-    public interface UpdateStatus{
+    public interface UpdateStatus {
         /**
          * 此版本强制更新
          */
@@ -16,17 +15,18 @@ public class MApplication extends Application {
         /**
          * 此版本可选更新
          */
-        int NO_FORCE=1;
+        int NO_FORCE = 1;
     }
 
 
     private static Context instance;
+
     public static Context getInstance() {
-        return  instance;
+        return instance;
     }
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        instance = this;
+
+    public static void init(Context context) {
+        instance = context;
     }
+
 }
